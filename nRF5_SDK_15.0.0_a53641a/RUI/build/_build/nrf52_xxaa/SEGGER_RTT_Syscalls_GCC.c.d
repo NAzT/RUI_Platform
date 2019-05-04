d2573154fa1aaaf1cdc019e5348c99623ca2f028
Nat <nat@cmmc.io>
2019-05-05 00:02:12 +0700

    remove _build

--- nRF5_SDK_15.0.0_a53641a/RUI/build/_build/nrf52_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
+++
@@ -1,5 +0,0 @@
-_build/nrf52_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
- ../../external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
- ../../RUI/Source/config/52832_board/itracker/sdk_config.h
-
-../../RUI/Source/config/52832_board/itracker/sdk_config.h:
--- nRF5_SDK_15.0.0_a53641a/RUI/build/_build/nrf52_xxaa/gcc_startup_nrf52.S.d
+++
@@ -1,2 +0,0 @@
-_build/nrf52_xxaa/gcc_startup_nrf52.S.o: \
- ../../modules/nrfx/mdk/gcc_startup_nrf52.S
