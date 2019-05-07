c5b85e4f608d42982ddab76151a6d52aca867a8b
Nat <nat@cmmc.io>
2019-05-08 06:23:11 +0700

    untrack Output dir.

--- nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/pca10040/s132/ses/Output/ble_app_hrs_freertos_pca10040_s132 Debug/Obj/uECC.d
+++
@@ -1,9 +0,0 @@
-Output/ble_app_hrs_freertos_pca10040_s132\ Debug/Obj/uECC.o: \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/uECC.c \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/uECC.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.16/include/stdint.h \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/uECC_vli.h \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/types.h \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/platform-specific.inc \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/asm_arm.inc \
- /Users/nat/111_RUI_Platform_Firmware_GCC_MAY/nRF5_SDK_15.0.0_a53641a/examples/cobike/ble_app_hrs_freertos/Source/external/micro-ecc/curve-specific.inc
